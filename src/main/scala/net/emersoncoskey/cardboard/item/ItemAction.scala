46d1229a6106f27cccae47c689f796e7171eb2a6
TheCostco <56370779+EmersonCoskey@users.noreply.github.com>
2022-01-10 17:12:49 -0800

    start making ItemAction monad

---
+++ src/main/scala/net/emersoncoskey/cardboard/item/ItemAction.scala
@@ -0,0 +1,26 @@
+package net.emersoncoskey.cardboard.item
+
+import cats.Monad
+import net.minecraft.world.item.Item
+import net.minecraftforge.registries.{DeferredRegister, IForgeRegistry, IForgeRegistryEntry}
+
+class ItemAction[+A] private(a: => A) {
+	def run: A = a
+}
+
+object ItemAction {
+	implicit object ItemActionMonad extends Monad[ItemAction] {
+		override def pure[A](a: A): ItemAction[A] = ItemAction(a)
+
+		override def flatMap[A, B](fa: ItemAction[A])(f: A => ItemAction[B]): ItemAction[B] = ItemAction {
+			val res1 = fa.run
+			f(res1).run
+		}
+
+		override def tailRecM[A, B](a: A)(f: A => ItemAction[Either[A, B]]): ItemAction[B] = ??? //don't use this, dunno why it's here
+	}
+
+	def apply[A](a: => A): ItemAction[A] = new ItemAction(a)
+
+	def register[A <: Item](item: A): ItemAction[A] = ???
+}
