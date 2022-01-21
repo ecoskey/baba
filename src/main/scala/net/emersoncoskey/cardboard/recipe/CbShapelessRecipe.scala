57089b4bc1a888d84724749c0c48a94774467fa3
Emerson Coskey <56370779+EmersonCoskey@users.noreply.github.com>
2022-01-20 22:48:05 -0800

    continuing work on shapeless recipe declarations

--- src/main/scala/net/emersoncoskey/cardboard/recipe/CbShapelessRecipe.scala
+++ src/main/scala/net/emersoncoskey/cardboard/recipe/CbShapelessRecipe.scala
@@ -1,16 +1,33 @@
 package net.emersoncoskey.cardboard.recipe
 
 import cats.data.NonEmptyList
+import com.google.gson.JsonObject
+import net.minecraft.data.recipes.FinishedRecipe
+import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.item.Item
-import net.minecraft.world.item.crafting.Ingredient
+import net.minecraft.world.item.crafting.{Ingredient, RecipeSerializer}
 
 
-case class CbShapelessRecipe(ingredients: NonEmptyList[(Ingredient, Int)], result: Item, count: Int)
+case class CbShapelessRecipe(
+	ingredients: NonEmptyList[(Ingredient, Int)],
+	result     : Item,
+	count      : Int
+) extends FinishedRecipe {
+	override def serializeRecipeData(pJson: JsonObject): Unit = ???
+
+	override def getId: ResourceLocation = ???
+
+	override def getType: RecipeSerializer[_] = RecipeSerializer.SHAPELESS_RECIPE
+
+	override def serializeAdvancement(): JsonObject = ???
+
+	override def getAdvancementId: ResourceLocation = ???
+}
 
 object CbShapelessRecipe {
 	def apply(firstIngredient: (Ingredient, Int), restIngredients: (Ingredient, Int)*)
-	         (count: Int)(result: Item): CbShapelessRecipe =
-		new CbShapelessRecipe(NonEmptyList.of(firstIngredient, restIngredients:_*), result, count)
+	  (count: Int)(result: Item): CbShapelessRecipe =
+		new CbShapelessRecipe(NonEmptyList.of(firstIngredient, restIngredients: _*), result, count)
 
 	/*def apply(firstIngredient: (Item, Int), restIngredients: (Item, Int)*)
 	         (count: Int)(result: Item): CbShapelessRecipe = {
