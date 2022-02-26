a4ec474a3082b76cba56f0b236a4e9df52f46e30
Emerson Coskey <56370779+EmersonCoskey@users.noreply.github.com>
2022-02-26 15:29:18 -0800

    removed newline

--- src/main/scala/net/emersoncoskey/cardboard/registry/dsl/item/ItemMods.scala
+++ src/main/scala/net/emersoncoskey/cardboard/registry/dsl/item/ItemMods.scala
@@ -7,6 +7,5 @@ import net.minecraft.world.item.Item
 import net.minecraftforge.forge.event.lifecycle.GatherDataEvent
 
 object ItemMods {
-
 	def recipes(first: Item => CbRecipe, rest: (Item => CbRecipe)*): ModDecMod[Item] = new RecipesMod(first :: rest.toList)
 }
