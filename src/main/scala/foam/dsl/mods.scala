d10659872fe5d99f91e1e4c08b945856d9947134
Emerson Coskey <56370779+EmersonCoskey@users.noreply.github.com>
2022-03-17 10:43:52 -0700

    change dsl structure

---
+++ src/main/scala/foam/dsl/mods.scala
@@ -0,0 +1,5 @@
+package foam.dsl
+
+import foam.registry.dsl.AllDecMods
+
+object mods extends AllDecMods
--- src/main/scala/foam/dsl/package.scala
+++
@@ -1,8 +0,0 @@
-package foam
-
-import foam.registry.dsl.AllDecMods
-
-package object dsl {
-	object mods extends AllDecMods
-	//todo: add datagen dsl
-}
