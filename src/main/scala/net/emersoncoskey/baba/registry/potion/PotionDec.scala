5a7b5de4332844f29926f7b34e8e86618a9e2b79
Emerson Coskey <56370779+EmersonCoskey@users.noreply.github.com>
2022-03-22 09:26:54 -0700

    change declaration for potions

--- src/main/scala/net/emersoncoskey/baba/registry/potion/PotionDec.scala
+++ src/main/scala/net/emersoncoskey/baba/registry/potion/PotionDec.scala
@@ -7,16 +7,13 @@ import net.minecraft.world.item.alchemy.Potion
 import java.util.function.Supplier
 
 /** Used to declare a [[Potion]] to be added to the game */
-class PotionDec[+P <: Potion] private(
-	val name   : String,
-	ctor   : Seq[MobEffectInstance] => P,
-	effects: Seq[MobEffectInstance]
+class PotionDec private(
+	val name: String,
+	effects : Seq[MobEffectInstance]
 ) extends RegDec[Potion] {
-	lazy val supplier: Supplier[Potion] = () => ctor(effects)
+	lazy val supplier: Supplier[Potion] = () => new Potion(effects:_*)
 }
 
 object PotionDec {
-	def apply(name: String, effects: MobEffectInstance*): PotionDec[Potion] = new PotionDec[Potion](name, new Potion(_: _*), effects)
-
-	def apply[P <: Potion](name: String, ctor: Seq[MobEffectInstance] => P, effects: MobEffectInstance*) = new PotionDec[P](name, ctor, effects)
+	def apply(name: String, effects: MobEffectInstance*): PotionDec = new PotionDec(name, effects)
 }
